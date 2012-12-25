ae9238870d96ccc3b05ceb5964668218ae2c7b46
rynkjm <rynkjm@gmail.com>
2012-12-26 00:10:13 +0900

    fixed #3

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,11 +2,29 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+import models._
 
 object Application extends Controller {
   
+  val tweetForm = Form(
+    "content" -> nonEmptyText(maxLength = 140)
+  )
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    Ok(views.html.index( tweetForm, Tweet.all() ))
   }
-  
+
+
+  def tweet = Action { implicit request =>
+    tweetForm.bindFromRequest.fold(
+      errors => BadRequest(views.html.index(errors, Tweet.all())) ,
+      content => {
+        Tweet.create(content)
+        Redirect(routes.Application.index)
+      }
+    )
+  }
+
 }
